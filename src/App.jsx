b0af7aa896b5652b5e3bbd9d2fb90b10870710b2
Joseph Wang <jgwang@ad.unc.edu>
2023-05-11 09:01:22 -0400

    lksdjf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Meme from './components/Meme'
 import './App.css'
 
 function App() {
   return (
     <>
       <Header />
+      <Meme />
     </>
   )
 }
